bfee589f6b61ce622aef049909c042fc37e11986
NadielySilva <nady.ceng@gmail.com>
2022-01-27 12:52:12 -0300

    First commit
    using GH Desktop

---
+++ script.js
@@ -0,0 +1,29 @@
+//crie uma variável que receba um nome
+var nam;
+nam = 'Nady';
+
+//crie uma variável que receba um número
+var num;
+num = 23;
+
+//Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+var curYear;
+curYear = 2022;
+
+var sub;
+sub = curYear-num;
+
+// Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+console.log(curYear+num);
+
+//Mostre no console a mensagem "Estou mexendo no console.log";
+console.log("Estou mexendo no console.log");
+
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console;
+var quartaFeira;
+quartaFeira = false;
+
+console.log(quartaFeira)
+
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+console.log(typeof quartaFeira);
